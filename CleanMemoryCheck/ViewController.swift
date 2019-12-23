0643f5c6f311deddad54283d388f6b7896d088d1
Ashok Matoria <ashok.matoria@sap.com>
2019-12-23 12:47:10 -0500

    setup uitextfield and wkwebview

--- CleanMemoryCheck/ViewController.swift
+++ CleanMemoryCheck/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let button = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(action))
+        navigationItem.rightBarButtonItem = button
     }
-
+    
+    @objc func action() {
+        autoreleasepool {
+            let alert = UIAlertController(title: "Enter any text", message: nil, preferredStyle: .alert)
+            alert.addTextField { (textField) in
+                
+            }
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
+class TextFieldViewController: UIViewController {
+    
+}
+
+class WebViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let url = URL(string: "https://google.com") else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
