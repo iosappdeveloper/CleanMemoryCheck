9848792c471394036ba93e6079e11f50c5821a30
Ashok Matoria <ashok.matoria@sap.com>
2019-12-30 14:59:03 -0500

    private browsing webview setup, minor refactoring

--- CleanMemoryCheck/ViewController.swift
+++ CleanMemoryCheck/ViewController.swift
@@ -9,39 +9,64 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         let button = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(action))
         navigationItem.rightBarButtonItem = button
     }
-    
-    @objc func action() {
-        autoreleasepool {
-            let alert = UIAlertController(title: "Enter any text", message: nil, preferredStyle: .alert)
-            alert.addTextField { (textField) in
-                
-            }
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            present(alert, animated: true, completion: nil)
+}
+
+private extension ViewController {
+    @objc private func action() {
+        let alert = UIAlertController(title: "Enter any text", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Sensitive user text"
         }
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
-
 }
 
-class TextFieldViewController: UIViewController {
+final class TextFieldViewController: UIViewController {
     
 }
 
-class WebViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
+final class WebViewController: UIViewController {
+    private var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        // Configure with in memory data store. This makes WebKit treat web page caches, user data etc similar to private browsing.
+        configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSubviews()
+        
+        // Load web page
         guard let url = URL(string: "https://google.com") else { return }
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+    
+}
+
+private extension WebViewController {
+    func setupSubviews() {
+        // Add the web view layout edge to edge
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let layoutGuide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor)
+        ])
     }
 }
